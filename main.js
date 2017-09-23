a41aac5d6ebb54bbcde0a029c33d1e7718b282d1
Anders Pitman <tapitman11@gmail.com>
2017-09-23 19:07:10 +0000

    Add dragon animation

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 /*globals Phaser */
 
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+(function() {
+    "use strict"
 
-function preload() {
-}
-
-function create() {
-}
-
-function update() {
-}
+    var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+    var frameWidthPixels = 128;
+    var frameHeightPixels = 128;
+    var frameCount = 8;
+    
+    function preload() {
+        
+        game.load.spritesheet('dragon', 'assets/dragon.png', frameWidthPixels, frameHeightPixels, frameCount);
+    }
+    
+    function create() {
+        
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        
+        var dragon = game.add.sprite(0, 0, 'dragon');
+        dragon.animations.add('fly', [0,1,2,3,4,5,6,7], 10, true);
+        dragon.animations.play('fly');
+    }
+    
+    function update() {
+    }
+    
+}());
